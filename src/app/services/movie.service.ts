ef0006fc8e9b156a2724706c3e4439c096732183
WilliamMelgoza <wmelgoza@protonmail.com>
2020-04-24 20:16:19 -0500

    Created enum SearchType in movieService

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +15,14 @@ export class MovieService {
   url =  'http://www.omdbapi.com/';
   apiKey = '12ba6da9';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchData() {
+
+  }
+
+  getDetails() {
+
+  }
+  
 }
