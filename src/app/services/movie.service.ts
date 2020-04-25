234011608a8368c1e90a4fa539f8e4085721e79d
WilliamMelgoza <wmelgoza@protonmail.com>
2020-04-24 20:11:39 -0500

    Added url and api key to movieService

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  url =  'http://www.omdbapi.com/';
+  apiKey = '12ba6da9';
 
   constructor() { }
 }
