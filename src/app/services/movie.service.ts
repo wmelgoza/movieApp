bc49102862a937900cbb654a3a8306ba1980c58e
WilliamMelgoza <wmelgoza@protonmail.com>
2020-04-24 20:29:56 -0500

    Created 2 functions defining movieService

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 export enum SearchType {
   all = '',
@@ -17,12 +19,20 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
-  searchData() {
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`)
+    .pipe(
+      map(results => {
+        console.log('RAW: ', results);
+        return results['Search'];
+      })
+    );
 
   }
 
-  getDetails() {
+  getDetails(id) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
 
   }
-  
+
 }
