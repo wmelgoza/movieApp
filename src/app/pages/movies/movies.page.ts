fc506e069b6ffa5fab62c29eee31222936b106f6
WilliamMelgoza <wmelgoza@protonmail.com>
2020-04-24 20:50:18 -0500

    Added searchBar, iterate results as observable, reroute to details

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SearchType, MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm = '';
+  type: SearchType = SearchType.all;
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+  }
+
+  searchChanged() {
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+
+    // this.results.subscribe(res => {
+
+    // })
   }
 
 }
