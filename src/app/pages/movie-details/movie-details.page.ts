60c49da6f99e5f70c6eb0bdb6c50174518f38f40
WilliamMelgoza <wmelgoza@protonmail.com>
2020-04-24 21:57:28 -0500

    movieDetails set with rating, director, plot, actors, and poster with icons

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  information = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.movieService.getDetails(id).subscribe(result => {
+      console.log('details: ', result);
+      this.information = result;
+    });
   }
+
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
+
+
 
 }
